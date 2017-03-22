ca1748dae6299bc26e7830f77bc56a73d6be0740
Hector Bustillos <hecbuma@gmail.com>
2017-03-22 17:00:40 -0600

    disbale airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -5,4 +5,4 @@ Airbrake.configure do |c|
   c.project_key         = ENV['AIRBRAKE_API_KEY']
   c.root_directory      = Rails.root
   c.blacklist_keys      = [/password/i]
-end if Rails.env.production?
+end unless Rails.env.production?
